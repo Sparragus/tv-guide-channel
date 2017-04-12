1fe1ada587a5a02cd00d0764e47276e666636626
Richard Kaufman <richardbkaufman@gmail.com>
2017-04-11 23:56:12 -0500

    Installs saga middleware

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,9 +1,19 @@
 import { createStore, applyMiddleware, compose } from 'redux'
+import createSagaMiddleware from 'redux-saga'
 import thunk from 'redux-thunk'
 
 import reducers from '../modules'
+import rootSaga from '../sagas'
+
+const sagaMiddleware = createSagaMiddleware()
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-const enhancers = composeEnhancers(applyMiddleware(thunk))
+const enhancers = composeEnhancers(
+  applyMiddleware(sagaMiddleware, thunk)
+)
 
-export default createStore(reducers, enhancers)
+const store = createStore(reducers, enhancers)
+
+sagaMiddleware.run(rootSaga)
+
+export default store
